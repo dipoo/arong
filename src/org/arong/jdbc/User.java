ba60a191a8c5eee923dcfe7e5b11d00b74added0
dipoo <120826531@qq.com>
2014-07-02 22:17:02 +0800

    jdbc util

---
+++ src/org/arong/jdbc/User.java
@@ -0,0 +1,62 @@
+package org.arong.jdbc;
+
+import java.util.Date;
+
+import org.arong.utils.JsonUtil;
+
+public class User {
+
+	private int id;
+	private String name;
+	private String password;
+	private Date regdate;
+	private User user;
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public User() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Date getRegdate() {
+		return regdate;
+	}
+
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+
+	@Override
+	public String toString() {
+		return JsonUtil.toString(this, "user");
+	}
+}
